da4c97b08ee4962d762aee2c8ffe65407565075b
Happy Tidjani <tidjani.happy@gmail.com>
2026-02-23 23:58:58 +0100

    feat: complete Sentry integration with profiling and logging mirror

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,6 +4,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Support\Facades\Route;
+use Sentry\Laravel\Integration;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -52,4 +53,6 @@ return Application::configure(basePath: dirname(__DIR__))
         $exceptions->shouldRenderJsonWhen(function ($request, Throwable $e) {
             return $request->expectsJson() || $request->is('api/*');
         });
+
+        Integration::handles($exceptions);
     })->create();
