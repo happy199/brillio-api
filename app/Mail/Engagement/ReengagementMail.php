683d47d45e852abe960284e08eaf7293a591eb01
Happy Tidjani <tidjani.happy@gmail.com>
2026-03-18 16:30:20 +0100

    style: restore ShouldQueue to ReengagementMail and final pint cleanup

--- app/Mail/Engagement/ReengagementMail.php
+++ app/Mail/Engagement/ReengagementMail.php
@@ -4,12 +4,13 @@ namespace App\Mail\Engagement;
 
 use App\Models\User;
 use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
 
-class ReengagementMail extends Mailable
+class ReengagementMail extends Mailable implements ShouldQueue
 {
     use Queueable, SerializesModels;
 
