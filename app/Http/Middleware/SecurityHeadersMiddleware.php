c96c9791cf834bc1143cda3e20f56a29fdf0cf25
Happy Tidjani <tidjani.happy@gmail.com>
2026-03-26 00:45:27 +0100

    fix: add missing Vite facade import in SecurityHeadersMiddleware

--- app/Http/Middleware/SecurityHeadersMiddleware.php
+++ app/Http/Middleware/SecurityHeadersMiddleware.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Vite;
 use Symfony\Component\HttpFoundation\Response;
 
 class SecurityHeadersMiddleware
