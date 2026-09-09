2a9ce192fc5583fa53080ae0e48a38c033a8bade
Happy Tidjani <tidjani.happy@gmail.com>
2026-09-09 22:19:29 +0200

    fix(ui): update button label to 'Évaluer mon CV' removing 'avec l'IA'

--- resources/views/jeune/outils.blade.php
+++ resources/views/jeune/outils.blade.php
@@ -124,7 +124,7 @@
                     <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-8l-4-4m0 0L8 8m4-4v12" />
                     </svg>
-                    <span>Lancer l'évaluation IA de mon CV</span>
+                    <span>Évaluer mon CV</span>
                 </button>
             </div>
         @else
